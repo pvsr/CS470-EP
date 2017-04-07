3e262f3e0225166bd4da22e47efdee47318d1fb3
Peter Rice <ricepv@dukes.jmu.edu>
2017-04-07 14:00:02 -0400

    use a real arg parser for vf script

--- generator.py
+++ generator.py
@@ -1,45 +1,33 @@
 import random
 import sys
+import argparse
 
-argc = len(sys.argv)
-if argc < 2:
-    print "Usage: python generator.py FILENAME NUM_CANDS MIN_VOTES MAX_VOTES"
-    print "  FILENAME is required"
-    print "  NUM_CANDS defaults to 6"
-    print "  MIN_VOTES defaults to 10"
-    print "  MAX_VOTES defaults to MIN_VOTES + 1000"
-    sys.exit(0)
+parser = argparse.ArgumentParser(description="generate a random votefile")
+parser.add_argument("-n, --num_cands", metavar="N", type=int,
+                    dest="cands", default=6, help="number of candidates, default 6")
+parser.add_argument("-m, --min_votes", metavar="N", type=int,
+                    dest="min", default=0, help="minimum number of votes, default 0")
+parser.add_argument("-M, --max_votes", metavar="N", type=int,
+                    dest="max", default=-1, help="maximum number of votes, default min_votes + 1000")
+parser.add_argument("-p, --preferential", dest="pref", action="store_true",
+                    help="whether to generate full preferential votes")
+parser.add_argument("filename", metavar="FILE", type=str,
+                    help="output file")
+args = parser.parse_args()
 
-filename = sys.argv[1]
+if args.max == -1:
+    args.max = args.min + 1000
 
-if argc > 2:
-    cands = int(sys.argv[2])
-else:
-    cands = 6
-
-if argc > 3:
-    minvote = int(sys.argv[3])
-else:
-    minvote = 10
-
-if minvote < 0:
-    minvote = 10
-
-if argc > 4:
-    maxvote = int(sys.argv[4])
-else:
-    maxvote = minvote + 1000
-
-if maxvote < 0 or maxvote < minvote:
-    maxvote = minvote + 1000
-
-file = open(filename, 'w')
-file.write('n {0}\n\n'.format(cands))
+file = open(args.filename, 'w')
+file.write('n {0}\n\n'.format(args.cands))
 file.write('c\n')
 
-for i in range(0, cands):
-    votes = random.randrange(minvote, maxvote)
-    file.write('{0} {1}\n'.format(votes, i))
+if args.pref:
+    print "uhh"
+else:
+    for i in range(0, args.cands):
+        votes = random.randrange(args.min, args.max)
+        file.write('{0} {1}\n'.format(votes, i))
 
 file.write('!\n')
 file.close
